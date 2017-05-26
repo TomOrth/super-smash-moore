5c7eed7ded467b01a0920d6389c572b06e6a9b64
gregoryhazen <gregoryhazen@gmail.com>
2017-05-26 09:22:16 -0400

    Player Class

---
+++ src/com/tom/greg/matt/smash/Entity.java
@@ -0,0 +1,91 @@
+package com.tom.greg.matt.smash;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public abstract class Entity {
+
+	public int x, y;
+	public int width, height;
+	public double vX, vY;
+
+	public Entity(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	public Rectangle getBounds() {
+		return new Rectangle(getX(), getY(), getWidth(), getHeight());
+	}
+
+	public Rectangle getBoundsTop() {
+		return new Rectangle(getX() + 10, getY(), getWidth() - 20, 5);
+	}
+
+	public Rectangle getBoundsBottom() {
+		return new Rectangle(getX() + 10, getY() + getHeight() - 5, getWidth() - 20, 5);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public double getvX() {
+		return vX;
+	}
+
+	public void setvX(double vX) {
+		this.vX = vX;
+	}
+
+	public double getvY() {
+		return vY;
+	}
+
+	public void setvY(double vY) {
+		this.vY = vY;
+	}
+
+	public Rectangle getBoundsLeft() {
+		return new Rectangle(getX(), getY() - 10, 5, getHeight() - 20);
+	}
+
+	public Rectangle getBoundsRight() {
+		return new Rectangle(getX() + getWidth() - 5, getY() + 10, 5, getHeight() - 20);
+	}
+
+	public abstract void render(Graphics g);
+
+	public abstract void tick();
+	
+}
--- src/com/tom/greg/matt/smash/Main.java
+++ src/com/tom/greg/matt/smash/Main.java
@@ -1,5 +1,5 @@
 package com.tom.greg.matt.smash;
 
 public class Main {
-
+	
 }
