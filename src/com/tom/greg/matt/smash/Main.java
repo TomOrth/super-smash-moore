0d40491523f5f60aab1c1b859d0804298eb32135
atf1999 <torth212@gmail.com>
2017-05-26 09:23:36 -0400

    Added skeleton main class

--- src/com/tom/greg/matt/smash/Main.java
+++ src/com/tom/greg/matt/smash/Main.java
@@ -1,5 +1,88 @@
 package com.tom.greg.matt.smash;
 
-public class Main {
+import java.awt.Canvas;
+import java.awt.Dimension;
 
+public class Main extends Canvas implements Runnable {
+	
+	public static final int WIDTH = 150, HEIGHT = WIDTH *5/7, SCALE = 3;
+	public static boolean running = false, gameOver = false;
+	private Thread thread;
+	private int seconds = 0;
+	
+	private void init() {
+		
+	}
+	
+	private void render() {
+		
+	}
+	
+	private void tick() {
+		
+	}
+	
+	public Main() {
+		Dimension size = new Dimension(WIDTH*SCALE, HEIGHT*SCALE);
+		setPreferredSize(size);
+		setMaximumSize(size);
+		setMinimumSize(size);
+	}
+	
+	private synchronized void start() {
+		if (running) {
+			return;
+		}
+		running = true;
+		thread = new Thread(this, "Thread");
+		thread.start();
+	}
+	
+	private synchronized void stop() {
+		if (!running) {
+			return;
+		}
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run() {
+
+		init();
+		requestFocus();
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		double delta = 0.0;
+		double ns = 1000000000.0 / 60.0;
+		int frames = 0;
+		int ticks = 0;
+		
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				tick();
+				ticks++;
+				delta--;
+			}
+			render();
+			frames++;
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				//System.out.println(frames + " fps " + ticks + " updates/sec ");
+				if(!gameOver) seconds += ticks/60;
+				frames = 0;
+				ticks = 0;
+			}
+		}
+		
+		stop();
+		
+	}
 }
